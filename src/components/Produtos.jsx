0bc0fd838f841096bd292ac668bd47337b6ac4d9
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-09 15:47:28 -0300

    pulando linha

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -26,6 +26,7 @@ const Produtos = () => {
         <div className="cardProduto">
           <img src={produt2} alt="produto 2" className="imgProduct"/>
           <p className="precoProdutos">$40</p>
+          <br />
           <p className="nomeDoProduto">Whole GrainBread</p>
         </div>
 
