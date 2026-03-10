9430e2026698691c70c170d86e7377ad667b0206
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 11:49:18 -0300

    descriçao prato

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -9,14 +9,50 @@ import produt5 from "../img/product5.png";
 import produt6 from "../img/product6.png";
 
 const Produtos = () => {
-  // Lista de produtos para evitar repetição de HTML
+
   const listaProdutos = [
-    { id: 1, nome: "Whole Grain Bread", preco: "$40", img: produt1 },
-    { id: 2, nome: "Whole Grain Bread", preco: "$40", img: produt2 },
-    { id: 3, nome: "Whole Grain Bread", preco: "$40", img: produt3 },
-    { id: 4, nome: "Whole Grain Bread", preco: "$40", img: produt4 },
-    { id: 5, nome: "Whole Grain Bread", preco: "$40", img: produt5 },
-    { id: 6, nome: "Whole Grain Bread", preco: "$40", img: produt6 },
+    {
+      id: 1,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt1,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 2,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt2,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 3,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt3,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 4,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt4,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 5,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt5,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 6,
+      nome: "Whole Grain Bread",
+      preco: "$40",
+      img: produt6,
+      descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
   ];
 
   return (
@@ -26,12 +62,24 @@ const Produtos = () => {
       <div className="flexbox">
         {listaProdutos.map((produto) => (
           <div className="cardProduto" key={produto.id}>
-            <img src={produto.img} alt={produto.nome} className="imgProduct" />
+
+            <img
+              src={produto.img}
+              alt={produto.nome}
+              className="imgProduct"
+            />
+
             <p className="nomeDoProduto">{produto.nome}</p>
+
+            <p className="descricaoProduto">
+              {produto.descricao}
+            </p>
+
             <div className="infoPreco">
               <p className="precoProdutos">{produto.preco}</p>
               <button className="btnCarrinho">ADD</button>
             </div>
+
           </div>
         ))}
       </div>
