60cb74135b028efae36ed53dafa75d924330a512
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 11:47:19 -0300

    array

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -9,70 +9,31 @@ import produt5 from "../img/product5.png";
 import produt6 from "../img/product6.png";
 
 const Produtos = () => {
+  // Lista de produtos para evitar repetição de HTML
+  const listaProdutos = [
+    { id: 1, nome: "Whole Grain Bread", preco: "$40", img: produt1 },
+    { id: 2, nome: "Whole Grain Bread", preco: "$40", img: produt2 },
+    { id: 3, nome: "Whole Grain Bread", preco: "$40", img: produt3 },
+    { id: 4, nome: "Whole Grain Bread", preco: "$40", img: produt4 },
+    { id: 5, nome: "Whole Grain Bread", preco: "$40", img: produt5 },
+    { id: 6, nome: "Whole Grain Bread", preco: "$40", img: produt6 },
+  ];
+
   return (
     <div className="produtosVendas">
       <h2 className="tituloProdutos">Top Products</h2>
 
       <div className="flexbox">
-        {/* Card 1 */}
-        <div className="cardProduto">
-          <img src={produt1} alt="produto 1" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
+        {listaProdutos.map((produto) => (
+          <div className="cardProduto" key={produto.id}>
+            <img src={produto.img} alt={produto.nome} className="imgProduct" />
+            <p className="nomeDoProduto">{produto.nome}</p>
+            <div className="infoPreco">
+              <p className="precoProdutos">{produto.preco}</p>
+              <button className="btnCarrinho">ADD</button>
+            </div>
           </div>
-        </div>
-
-        {/* Card 2 */}
-        <div className="cardProduto">
-          <img src={produt2} alt="produto 2" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
-          </div>
-        </div>
-
-        {/* Card 3 */}
-        <div className="cardProduto">
-          <img src={produt3} alt="produto 3" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
-          </div>
-        </div>
-
-        {/* Card 4 */}
-        <div className="cardProduto">
-          <img src={produt4} alt="produto 4" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
-          </div>
-        </div>
-
-        {/* Card 5 */}
-        <div className="cardProduto">
-          <img src={produt5} alt="produto 5" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
-          </div>
-        </div>
-
-        {/* Card 6 */}
-        <div className="cardProduto">
-          <img src={produt6} alt="produto 6" className="imgProduct" />
-          <p className="nomeDoProduto">Whole Grain Bread</p>
-          <div className="infoPreco">
-            <p className="precoProdutos">$40</p>
-            <button className="btnCarrinho">ADD</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
