363debeec78cf991432767e6ed315bb77503aaa5
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-16 10:41:45 -0300

    img importacao

--- src/components/ExploreMore.jsx
+++ src/components/ExploreMore.jsx
@@ -5,6 +5,7 @@ import exploreMore1 from "../img/exploreMore1.png"
 import exploreMore2 from "../img/exploreMore2.png"
 import exploreMore3 from "../img/exploreMore3.png"
 import exploreMore4 from "../img/exploreMore4.png"
+import exploreMore5 from "../img/exploreMore5.png"
 
 
 const ExploreMore = () => {
