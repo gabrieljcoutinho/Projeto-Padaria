6047a58c51345d0ef831d29cfef78483ff6416a7
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-16 10:43:52 -0300

    img

--- src/components/ExploreMore.jsx
+++ src/components/ExploreMore.jsx
@@ -42,7 +42,7 @@ const ExploreMore = () => {
 
 
                                 <div className="pratos">
-                                <img src={exploreMore1} alt="" className="imgExploreMore" />
+                                <img src={exploreMore5} alt="" className="imgExploreMore" />
                                     </div>
 
                                     <div className="pratos">
