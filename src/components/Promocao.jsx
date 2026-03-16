e1816be936e1c45d0854bafcb7973978a0e75fdb
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-16 09:55:36 -0300

    quebra de linha

--- src/components/Promocao.jsx
+++ src/components/Promocao.jsx
@@ -6,7 +6,7 @@ const Promocao = () => {
   return (
     <div className='promocao'>
 
-        <h2 className='subTitulo'>20% Off  Your First Order</h2>
+        <h2 className='subTitulo'>20% Off  Your <br /> First Order</h2>
 
           <p className="promocaoDescricao">Suspendisse ar rhoncud nisl eu tempor urna. Curabitur vel bibenjgg</p>
 
