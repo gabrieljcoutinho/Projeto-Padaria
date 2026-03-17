c3ac541d4a80139742bfaa7d4a310adc31d8004f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-17 08:24:54 -0300

    lista

--- src/components/ExploreMore.jsx
+++ src/components/ExploreMore.jsx
@@ -21,6 +21,7 @@ const ExploreMore = () => {
                         <li className="listaExploreMoreFoods">Cake<a href="" className="linksTopicosComidaExploreMore"></a></li>
                         <li className="listaExploreMoreFoods">Muffins<a href="" className="linksTopicosComidaExploreMore"></a></li>
                         <li className="listaExploreMoreFoods">Croissant<a href="" className="linksTopicosComidaExploreMore"></a></li>
+                        <li className="listaExploreMoreFoods">bread<a href="" className="linksTopicosComidaExploreMore"></a></li>
 
                 </ul>
 
