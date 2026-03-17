eb700c08f72713791612da094427c9f1d7e2a41c
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-17 08:36:50 -0300

    sem redireiconamento

--- src/components/ExploreMore.jsx
+++ src/components/ExploreMore.jsx
@@ -19,7 +19,7 @@ const ExploreMore = () => {
                 <ul className="listaTopicosDeComidas">
 
                         <li className="listaExploreMoreFoods"><a href="#" className="linksTopicosComidaExploreMore">Cake</a></li>
-                        <li className="listaExploreMoreFoods"><a href="" className="linksTopicosComidaExploreMore">Muffins</a></li>
+                        <li className="listaExploreMoreFoods"><a href="#" className="linksTopicosComidaExploreMore">Muffins</a></li>
                         <li className="listaExploreMoreFoods"><a href="" className="linksTopicosComidaExploreMore">Croissant</a></li>
                         <li className="listaExploreMoreFoods"><a href="" className="linksTopicosComidaExploreMore">Bread</a></li>
                         <li className="listaExploreMoreFoods"><a href="" className="linksTopicosComidaExploreMore">Tart</a></li>
