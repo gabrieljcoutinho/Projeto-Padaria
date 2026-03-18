bde583c5845f25e8a59bcff46dfa8adfa54d64a5
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-18 10:27:10 -0300

    img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                                         <li><a href="http://"><img src={facebook} alt="" /></a></li>
                                         <li><a href="http://"><img src={pinterest} alt="" /></a></li>
                                         <li><a href="http://"><img src={zap} alt="" /></a></li>
-                                        <li><a href="http://"></a></li>
+                                        <li><a href="http://"><img src={instagram} alt="" /></a></li>
 
                           </ul>
 
